8a4905868a733f8ec6216ce6917bca934db7970e
Henao-Ceballos <fhenaoceballos@uiowa.edu>
2025-09-04 18:25:43 -0500

    > Update

--- R/generate_event_locations.R
+++ R/generate_event_locations.R
@@ -51,6 +51,18 @@
 #' @import purrr
 #' @import tigris
 #' @importfrom geodata population
+#' 
+#' @examples
+#' library(gdp)
+#' data(Stroke_Rate)
+#' population<-get_census_population()
+#' strokes<-generate_event_locations(rate=Stroke_Rate,
+#'                                  family = "negative_binomial",
+#'                                  population = population,
+#'                                  state_fip_code = "01",
+#'                                  control = list(size=10))
+#' 
+#' 
 #' @export
 
 
